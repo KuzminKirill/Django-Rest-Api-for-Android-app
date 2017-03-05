144a431270d7e3b799edf22a17c704519a20e657
KuzminKirill <wee3dood@gmail.com>
2017-03-05 22:57:02 +0300

    serializers for all models

--- apitry/serializers.py
+++ apitry/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from apitry.models import Course
+from apitry.models import Course, Theme, Test, TestPossibleAnswers, TestResults, UsersCourse
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -39,3 +39,27 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('id', 'start_at', 'finish_at', 'description', 'created_at', 'is_published')
+
+
+class ThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Theme
+        fields = ('id','course_id', 'title', 'description', 'theory')
+
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Test
+        fields = ('id', 'course_id', 'question')
+
+
+class TestPossibleAnswersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestPossibleAnswers
+        fields = ('id', 'test_id', 'answer', 'is_true')
+
+
+class TestResultsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestResults
+        fields = ('id', 'user_id', 'test_id', 'result')
