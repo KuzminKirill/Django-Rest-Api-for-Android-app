1a3dc6e90dce1b3548d3d0abcb977ca4564609e1
KuzminKirill <wee3dood@gmail.com>
2017-03-05 23:41:27 +0300

    add views

--- apitry/views.py
+++ apitry/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from apitry.serializers import UserSerializer, GroupSerializer
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from apitry.models import Course
+from apitry.serializers import CourseSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +26,59 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def course_list(request):
+    """
+    List all code courses, or create a new course.
+    """
+    if request.method == 'GET':
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CourseSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def course_detail(request, pk):
+    """
+    Retrieve, update or delete a code course.
+    """
+    try:
+        course = Course.objects.get(pk=pk)
+    except Course.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CourseSerializer(course)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CourseSerializer(course, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        course.delete()
+        return HttpResponse(status=204)
